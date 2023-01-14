42c1f0493c9ced3b39e25cdab68ff01199aae7f2
bc-dipesh <bcdipeshwork@gmail.com>
2023-01-14 09:52:42 -0500

    Add logic to post unique quote from a list of quotes

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import os
+import json
 import tweepy
+
+
+def get_daily_quote():
+    daily_quote = None
+
+    with open("quotes.json", "r+") as file:
+        quotes = json.load(file)
+        daily_quote = quotes[0]["quote"]
+        quotes.pop(0)
+        file.seek(0)
+        json.dump(quotes, file)
+        file.truncate()
+
+    return daily_quote
 
 
 def get_authorized_api():
@@ -11,16 +26,17 @@ def get_authorized_api():
     return api
 
 
-def post_tweet(api):
-    api.update_status("Hello World!2")
+def post_tweet(api, tweet):
+    api.update_status(tweet)
 
 
 if __name__ == "__main__":
-    # Get data from environment variables
+    # get data from environment variables
     API_KEY = os.getenv("TWITTER_API_KEY", "NOT FOUND")
     API_KEY_SECRET = os.getenv("TWITTER_API_KEY_SECRET", "NOT FOUND")
     ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN", "NOT FOUND")
     ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET", "NOT FOUND")
 
+    quote = get_daily_quote()
     api = get_authorized_api()
-    post_tweet(api)
+    post_tweet(api, quote)
