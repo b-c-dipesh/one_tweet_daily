1c860242f9cf69491a74c5bb440b6b3f27b8d855
bc-dipesh <bcdipeshwork@gmail.com>
2023-01-18 20:11:19 -0500

    Migrate data from .json to .sqlite

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import os
 import json
 import tweepy
+import sqlite3
+
+conn = sqlite3.connect('quotes.sqlite')
+cur = conn.cursor()
 
 
 def get_daily_quote():
     daily_quote = None
 
-    with open("quotes.json", "r+") as file:
-        quotes = json.load(file)
-        daily_quote = quotes[0]["quote"]
-        quotes.pop(0)
-        file.seek(0)
-        json.dump(quotes, file)
-        file.truncate()
+    cur.execute(
+        "SELECT * from Quotes WHERE is_posted = 0 LIMIT 1")
+    daily_quote = cur.fetchone()
 
     return daily_quote
 
@@ -27,7 +27,8 @@ def get_authorized_api():
 
 
 def post_tweet(api, tweet):
-    api.update_status(tweet)
+    api.update_status(tweet[1])
+    cur.execute("UPDATE Quotes SET is_posted = 1 WHERE id = ?", (tweet[0],))
 
 
 if __name__ == "__main__":
@@ -37,6 +38,24 @@ if __name__ == "__main__":
     ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN", "NOT FOUND")
     ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET", "NOT FOUND")
 
+    # Uncomment these lines if you are running this app for the first time
+
+    # cur.execute('''CREATE TABLE IF NOT EXISTS Quotes (
+    # "id" INTEGER NOT NULL UNIQUE,
+    # "quote" TEXT NOT NULL UNIQUE,
+    # "is_posted" NUMERIC NOT NULL DEFAULT 0,
+    # PRIMARY KEY ("id" AUTOINCREMENT))''')
+
+    # with open("quotes.json") as file:
+    #     quotes = json.load(file)
+
+    # for entry in quotes:
+    #     cur.execute("INSERT INTO Quotes (quote) VALUES (?)", (entry['quote'],))
+    # conn.commit()
+
     quote = get_daily_quote()
     api = get_authorized_api()
     post_tweet(api, quote)
+
+    conn.commit()
+    cur.close()
